b60d5e46ffee8eb0897cb345efe0207d36eb06df
Jack Thompson <54644847+jackt0310@users.noreply.github.com>
2020-11-16 19:56:10 -0500

    Basic player/camera movement and key animation with particles

---
+++ Maze Explorer/Assets/FollowPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    public Vector3 offset = new Vector3(0, 1, -10);
+    public Vector3 defaultOffset = new Vector3(0, 1, -10);
+    public float smoothFactor = 0.5f;
+    public bool lookAtPlayer = false;
+    public bool rotateAroundPlayer = true;
+    public float rotationSpeed = 5.0f;
+    public float heightVal = 5f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion turnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+        offset = turnAngle * offset;
+        Vector3 newPos = player.transform.position + offset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+        transform.LookAt(player.transform.position + new Vector3(0, 3, 0));
+    }
+}
---
+++ Maze Explorer/Assets/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Components
+    private Animator animator;
+    Rigidbody rb;
+    
+    // Stats
+    public float moveSpeed = 100f;
+    public float walkSpeed = 10f;
+    public float runSpeed = 20f;
+    public float turnSpeed = 2f;
+    public int jumps = 1;
+    public float jumpForce = 10f;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        GameObject.Find("/Main Camera").GetComponent<FollowPlayer>().player = gameObject;
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        float moveSpeed = 0;
+        float forwardAmt = 1.0f;
+        float moveVertical = 0.0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveSpeed = runSpeed;
+            moveVertical = 1.0f;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            moveSpeed = runSpeed;
+            moveVertical = -1.0f;
+        }
+
+        float moveHorizontal = 0.0f;
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveSpeed = runSpeed;
+            moveHorizontal = 1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveSpeed = runSpeed;
+            moveHorizontal = -1.0f;
+        }
+        float cameraRotY = GameObject.Find("/Main Camera").transform.localRotation.eulerAngles.y;
+
+        Vector3 movement = Quaternion.AngleAxis(cameraRotY, Vector3.up) * new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        if (moveSpeed > 0)
+        {
+            rb.MoveRotation(Quaternion.LookRotation(movement));
+        }
+
+        rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
