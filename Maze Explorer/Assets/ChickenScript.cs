eb974c56a4dbf66c2a3d9bb650ec6b63bd108319
Jack Thompson <54644847+jackt0310@users.noreply.github.com>
2020-11-24 21:12:17 -0500

    Created basic ome sentence dialogue system

---
+++ Maze Explorer/Assets/ChickenScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenScript : MonoBehaviour
+{
+    public DialogueScript dialogue;
+    public GameObject player;
+    public GameObject dots;
+    public Sprite talkSprite;
+    public string text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = GameObject.Find("/DialogueControl").GetComponent<DialogueScript>();
+        player = GameObject.Find("/knight");
+        dots.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < 8f)
+        {
+            dots.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.C) && !dialogue.open)
+            {
+                dialogue.OpenDialogue(talkSprite, text);
+            }
+        } else
+        {
+            dots.SetActive(false);
+        }
+    }
+}
---
+++ Maze Explorer/Assets/DialogueScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueScript : MonoBehaviour
+{
+    public GameObject diaPanel;
+    public Image diaPortrait;
+    public Text diaText;
+    public bool open;
+    public bool canMoveOn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        diaPanel = GameObject.Find("/Canvas/DialoguePanel");
+        diaPortrait = GameObject.Find("/Canvas/DialoguePanel/DiaBox/DiaPortrait").GetComponent<Image>();
+        diaText = GameObject.Find("/Canvas/DialoguePanel/DiaBox/DiaText").GetComponent<Text>();
+        diaPanel.SetActive(false);
+    }
+
+
+    public void OpenDialogue(Sprite picture, string text)
+    {
+        diaPanel.SetActive(true);
+        diaPortrait.sprite = picture;
+        diaText.text = text;
+        open = true;
+        Invoke("Continue", .1f);
+    }
+
+    public void Continue()
+    {
+        canMoveOn = true;
+    }
+
+    public void CloseDialogue()
+    {
+        diaPanel.SetActive(false);
+        open = false;
+        canMoveOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && open && canMoveOn)
+        {
+            CloseDialogue();
+        }
+    }
+}
